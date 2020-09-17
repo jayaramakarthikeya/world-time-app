3ae7f00636a3b4b946ceccc36689ebc89c347183
JayaramKarthik <karthikeyaj82@gmail.com>
2020-09-17 20:15:20 +0530

    resolved issue of bgImage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,70 +18,68 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: bgColor,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/$bgImage'),
-                fit: BoxFit.cover,
-              ),
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover,
             ),
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(0.0, 120.0, 0.0, 0.0),
-              child: Column(
-                children: <Widget>[
-                  FlatButton.icon(
-                    onPressed: () async {
-                      dynamic result =
-                          await Navigator.pushNamed(context, '/location');
-                      await Future.delayed(Duration(seconds: 1));
-                      if (result != null) {
-                        setState(() {
-                          data = {
-                            'time': result['time'],
-                            'location': result['location'],
-                            'isDayTime': result['isDayTime']
-                          };
-                        });
-                      }
-                    },
-                    icon: Icon(
-                      Icons.edit_location,
+          ),
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(0.0, 120.0, 0.0, 0.0),
+            child: Column(
+              children: <Widget>[
+                FlatButton.icon(
+                  onPressed: () async {
+                    dynamic result =
+                        await Navigator.pushNamed(context, '/location');
+                    await Future.delayed(Duration(seconds: 1));
+                    if (result != null) {
+                      setState(() {
+                        data = {
+                          'time': result['time'],
+                          'location': result['location'],
+                          'isDayTime': result['isDayTime']
+                        };
+                      });
+                    }
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: Colors.grey[200],
+                  ),
+                  label: Text(
+                    'Edit Location',
+                    style: TextStyle(
+                      fontSize: 17.0,
                       color: Colors.grey[200],
                     ),
-                    label: Text(
-                      'Edit Location',
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
                       style: TextStyle(
-                        fontSize: 17.0,
-                        color: Colors.grey[200],
+                        fontWeight: FontWeight.w400,
+                        fontSize: 30.0,
+                        color: Colors.white,
                       ),
                     ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 50.0,
+                    color: Colors.white,
                   ),
-                  SizedBox(height: 20.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        data['location'],
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 30.0,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20.0),
-                  Text(
-                    data['time'],
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 50.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
